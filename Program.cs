9e2ed9b094e01004ddf0ecf5f08984aba0977fa5
warith23 <ismailayomide32@gmail.com>
2022-06-18 11:48:27 +0100

    freelance

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+ 
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            /*
+            Console.WriteLine("Enter your first name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your favourite colour");
+            string favouriteColour = Console.ReadLine();
+            Console.WriteLine("Enter your favourite food");
+            string favouriteFood = Console.ReadLine();
+            Console.WriteLine("Enter your state of origin");
+            string stateofOrigin = Console.ReadLine();
+                      
+            Console.WriteLine("-------Question 2-------");
+            Console.WriteLine("=====\nWrite a Boolean expression that checks whether a\ngiven integer is divisible by both 5 and 7, without a remainder\n=====\n");
+            Console.Write("Enter the number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The number: {number2} is divisible by 5 and 7? {(number2 % 5 == 0) && (number2 % 7 == 0)}");
+            Console.WriteLine();
+
+
+
+            /*int num1, rem1;
+         Console.Write("\n\n");
+         Console.Write("Check whether a number is even or odd :\n");
+         Console.Write("---------------------------------------");
+         Console.Write("\n\n");
+         Console.Write("Input an integer : ");
+         num1= Convert.ToInt32(Console.ReadLine()); 
+         rem1 = num1 % 2;
+         if (rem1 == 0)
+         Console.WriteLine("{0} is an even integer.\n",num1);*/ 
+
+
+
+
+
+          
+
+
+
+
+
+
+
+        }
+
+         
+    }    
+}
+
+           
